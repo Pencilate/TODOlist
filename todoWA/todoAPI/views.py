b07b8764ca5861d7e5c778378ecd3fb7356f2515
Qianbo <pencilate@outlook.com>
2020-03-17 18:11:36 +0800

    Created Login and Logout Api Endpoint

--- todoWA/todoAPI/views.py
+++ todoWA/todoAPI/views.py
@@ -1,3 +1,39 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.http import HttpResponse,JsonResponse,HttpResponseNotFound
+from django.contrib.auth.models import User
+from todoAPI.models import Todo
+from django.core.serializers import serialize
+from django.core.exceptions import ObjectDoesNotExist
+from django.views import View
+import json
 
 # Create your views here.
+def loginView(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        # user = authenticate(request, username=username,password=password)
+
+        try:
+            user = User.objects.get(username=username,password=password)
+            if user is not None:
+                print("User Authenticated")
+                login(request,user)
+                return JsonResponse(data={"userId":user.id})
+        except ObjectDoesNotExist:
+            print("User Invalid")
+            return HttpResponse(json.dumps({"message":"You have entered invalid credential"}), content_type='application/json', status=401)
+    else:
+        return HttpResponse(json.dumps({"message":"Please use POST request header"}), content_type='application/json', status=405)
+
+def logoutView(request):
+    if request.method == 'POST':
+        if request.user.is_authenticated == True:
+            logout(request)
+            return JsonResponse(data={"message":"Successful logout"})
+        else:
+            return HttpResponse(json.dumps({"message":"Unauthorized Action. You are not logged in"}), content_type='application/json', status=401)
+    else:
+        return HttpResponse(json.dumps({"message":"Please use POST REST API to login"}), content_type='application/json', status=405)
+    
