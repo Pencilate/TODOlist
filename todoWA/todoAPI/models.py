dfd788a88b085255ee8b11f4ff21df4078f78d9b
Qianbo <pencilate@outlook.com>
2020-03-17 11:17:17 +0800

    Added Django to requiremnets.txt Added self object representation in Todo model

--- todoWA/todoAPI/models.py
+++ todoWA/todoAPI/models.py
@@ -10,3 +10,6 @@ class Todo(models.Model):
     title = models.CharField(max_length=80)
     description = models.CharField(max_length=1024)
     status = models.BooleanField()
+    
+    def __str__(self):
+        return self.title
