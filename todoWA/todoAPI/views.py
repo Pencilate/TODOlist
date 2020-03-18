f96e0e680216b4ca96be06497fd0b504333c2a82
Qianbo <pencilate@outlook.com>
2020-03-18 16:48:57 +0800

    Changed All HttpResponse to JSONResponses, Added BadRequest handling

--- todoWA/todoAPI/views.py
+++ todoWA/todoAPI/views.py
@@ -8,12 +8,19 @@ from django.core.exceptions import ObjectDoesNotExist
 from django.views import View
 import json
 import logging
+from django.utils.datastructures import MultiValueDictKeyError
 
 # Create your views here.
 def loginView(request):
     if request.method == 'POST':
-        username = request.POST['username']
-        password = request.POST['password']
+        try:
+            username = request.POST['username']
+            password = request.POST['password']
+        except MultiValueDictKeyError:
+            return JsonResponse({"message":"Ensure the request contains both 'username' and 'password' field"},status=400)
+        if not username or not password:
+            return JsonResponse({"message":"Ensure the 'username' and/or 'password' field in the data are not blank"},status=400)
+
         print("Username is "+username)
         print("Password is "+password)
         user = authenticate(request, username=username,password=password)
@@ -23,19 +30,19 @@ def loginView(request):
             return JsonResponse(data={"userId":user.id})
         else:
             print("User Invalid")
-            return HttpResponse(json.dumps({"message":"You have entered invalid credential"}), content_type='application/json', status=401)
+            return JsonResponse({"message":"You have entered invalid credential"}, status=401)
     else:
-        return HttpResponse(json.dumps({"message":"Please use POST request header"}), content_type='application/json', status=405)
+        return JsonResponse({"message":"Please use POST request header"}, status=405)
 
 def logoutView(request):
     if request.method == 'POST':
-        if request.user.is_authenticated == True:
+        if request.user.is_authenticated:
             logout(request)
             return JsonResponse(data={"message":"Successful logout"})
         else:
-            return HttpResponse(json.dumps({"message":"Unauthorized Action. You are not logged in"}), content_type='application/json', status=401)
+            return JsonResponse({"message":"Unauthorized Action. You are not logged in"}, status=401)
     else:
-        return HttpResponse(json.dumps({"message":"Please use POST REST API to login"}), content_type='application/json', status=405)    
+        return JsonResponse({"message":"Please use POST REST API to login"}, status=405)   
 
 class TodoController(View):  
     http_method_names = ['get','post','put','delete']
